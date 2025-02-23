400927a60f101a29abc7e454ec21a54056b001e0
badnumberr <ymniychel000@gmail.com>
2025-02-23 12:19:57 +0500

    map_api2

--- 2/main.py
+++ 2/main.py
@@ -5,20 +5,29 @@ pygame.init()
 
 W, H = 900, 700
 screen = pygame.display.set_mode((W, H))
-pygame.display.set_caption("1")
+pygame.display.set_caption("2")
 map_image = pygame.image.load("map.png")
 scale_factor = 1
+min_scale = 0.5
+max_scale = 3.0
 
-map_image = pygame.transform.scale(map_image, (int(map_image.get_width() * scale_factor),
-                                               int(map_image.get_height() * scale_factor)))
 map_x, map_y = (W - map_image.get_width()) // 2, (H - map_image.get_height()) // 2
-
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP:
+                scale_factor = min(scale_factor + 0.1, max_scale)
+            elif event.key == pygame.K_PAGEDOWN:
+                scale_factor = max(scale_factor - 0.1, min_scale)
+    scaled_map_image = pygame.transform.scale(map_image, (int(map_image.get_width() * scale_factor),
+                                                          int(map_image.get_height() * scale_factor)))
+
+    map_x, map_y = (W - scaled_map_image.get_width()) // 2, (H - scaled_map_image.get_height()) // 2
+
     screen.fill((255, 255, 255))
-    screen.blit(map_image, (map_x, map_y))
+    screen.blit(scaled_map_image, (map_x, map_y))
     pygame.display.flip()
