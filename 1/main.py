ca57025fb4e2e83cca1685022e416ce88f867286
badnumberr <ymniychel000@gmail.com>
2025-02-23 12:08:48 +0500

    map_api1

--- 1/main.py
+++ 1/main.py
@@ -3,15 +3,15 @@ import sys
 
 pygame.init()
 
-width, height = 900, 700
-screen = pygame.display.set_mode((width, height))
+W, H = 900, 700
+screen = pygame.display.set_mode((W, H))
 pygame.display.set_caption("1")
 map_image = pygame.image.load("map.png")
 scale_factor = 1
 
 map_image = pygame.transform.scale(map_image, (int(map_image.get_width() * scale_factor),
                                                int(map_image.get_height() * scale_factor)))
-map_x, map_y = (width - map_image.get_width()) // 2, (height - map_image.get_height()) // 2
+map_x, map_y = (W - map_image.get_width()) // 2, (H - map_image.get_height()) // 2
 
 
 while True:
